bb29e5d850f1fe29c7740407b6be7070215d2e20
Yuriy Ivaniv <ivanivyura@gmail.com>
2020-07-07 18:31:31 +0300

    added first card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import styles from "./Cards.module.css";
 
-const Cards = () => {
-  return(
-    <div>Cards</div>
-  )
-}
+const Cards = (props) => {
+  console.log(props);
+
+  return (
+    <div className={styles.module}>
+      <Grid container spacin={3} justify="center">
+		  <Grid item component={Card}>
+			<CardContent>
+				<Typography color='textSecondary' gutterButtom>Infected</Typography>
+				<Typography variant='h5' gutterButtom>Data</Typography>
+				<Typography color='textSecondary' >Real Data</Typography>
+				<Typography variant='body2'>Number of active cases of Covid-19</Typography>
+				
+			</CardContent>
+		  </Grid>
+	  </Grid>
+	  
+    </div>
+  );
+};
 
 export default Cards;
