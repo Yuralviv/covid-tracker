2ad65b83cad074a1c503bcf293cd0d68f0c744e5
Yuriy Ivaniv <ivanivyura@gmail.com>
2020-07-07 20:58:06 +0300

    added coutry list

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,8 @@ const url = "https://covid19.mathdro.id/api";
 export const fetchData = async () => {
   try {
     const {
-      data: { confirmed, recovered, deaths, lastUpdate }} = await axios.get(url);
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
 
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
@@ -13,15 +14,21 @@ export const fetchData = async () => {
 
 export const fetchDailyData = async () => {
   try {
-    const {data} = await axios.get(`${url}/daily`)
+    const { data } = await axios.get(`${url}/daily`);
     // console.log(data);
-    const modifiedData = data.map( (dailyData) => ({
+    const modifiedData = data.map((dailyData) => ({
       confirmed: dailyData.confirmed.total,
       deaths: dailyData.deaths.total,
       date: dailyData.reportDate,
-    }))
+    }));
 
     return modifiedData;
   } catch (error) {}
 };
 
+export const fetchCountries = async () => {
+  try {
+    const { data : {countries } } = await axios.get(`${url}/countries`);
+    return countries.map((country) => country.name)
+  } catch (error) {}
+};
--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { fetchCountries } from "../../api";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
 
 const CountryPicker = () => {
-  return(
-    <div>CountryPicker</div>
-  )
-}
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, [setCountries]);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+      </NativeSelect>
+    </FormControl>
+  );
+};
 
 export default CountryPicker;
