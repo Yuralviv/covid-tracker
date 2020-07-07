ee94a293d401da419706b1bb85514aba84d0cfc8
Yuriy Ivaniv <ivanivyura@gmail.com>
2020-07-07 18:35:42 +0300

    added all cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -8,17 +8,51 @@ const Cards = (props) => {
   return (
     <div className={styles.module}>
       <Grid container spacin={3} justify="center">
-		  <Grid item component={Card}>
-			<CardContent>
-				<Typography color='textSecondary' gutterButtom>Infected</Typography>
-				<Typography variant='h5' gutterButtom>Data</Typography>
-				<Typography color='textSecondary' >Real Data</Typography>
-				<Typography variant='body2'>Number of active cases of Covid-19</Typography>
-				
-			</CardContent>
-		  </Grid>
-	  </Grid>
-	  
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterButtom>
+              Infected
+            </Typography>
+            <Typography variant="h5" gutterButtom>
+              Data
+            </Typography>
+            <Typography color="textSecondary">Real Data</Typography>
+            <Typography variant="body2">
+              Number of active cases of Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterButtom>
+              Recovered
+            </Typography>
+            <Typography variant="h5" gutterButtom>
+              Data
+            </Typography>
+            <Typography color="textSecondary">Real Data</Typography>
+            <Typography variant="body2">
+              Number of recovered cases of Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterButtom>
+              Deaths
+            </Typography>
+            <Typography variant="h5" gutterButtom>
+              Data
+            </Typography>
+            <Typography color="textSecondary">Real Data</Typography>
+            <Typography variant="body2">
+              Number of deaths caused by Covid-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </div>
   );
 };
